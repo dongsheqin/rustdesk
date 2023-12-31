02d2f051acc4b061410f8255895d19ec098b76b1
dongsheqin <dongsheqin@aliyun.com>
2024-01-01 05:50:38 +0800

    this is the fifth fixed

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
@@ -19,7 +19,7 @@ import com.hjq.permissions.XXPermissions
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-
+import android.media.projection.MediaProjectionManager
 
 class MainActivity : FlutterActivity() {
     companion object {
